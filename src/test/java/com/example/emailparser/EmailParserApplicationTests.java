54e4605f446d536f8ab23c1d2417db2069735e1e
Liming <wooof21@gmail.com>
2025-07-23 17:13:31 -0400

    Add files via upload

---
+++ src/test/java/com/example/emailparser/EmailParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
