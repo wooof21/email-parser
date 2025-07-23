46898f73271bdb110297177cdfb256b0400a5e82
Liming <wooof21@gmail.com>
2025-07-23 17:12:41 -0400

    Add files via upload

---
+++ src/main/java/com/example/emailparser/EmailParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailParserApplication.class, args);
+	}
+
+}
